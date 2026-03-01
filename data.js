52ed7fc3215f4f1c1ac3fd0c6ed6e18bf7cd0a8b
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-02-28 21:40:03 -0500

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-lo-bun--m-hi-mu",
+      "name": "一、耶穌山園祈禱",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8763970383256172,
+          "pitch": 0.16735844249532406,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-chu-nh-n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8315975342972379,
+          "pitch": -0.1932127154048935,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌山園祈禱</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">隨後，耶穌同他們來到一個名叫革責瑪尼的莊園裏，便對門徒說：「你們坐在這裏，等我到那邊去祈禱。」遂帶了伯多祿和載伯德的兩個兒子同去，開始憂悶恐怖起來，對他們說：「我的心靈憂悶得要死，你們留在這裏同我一起醒寤罷！」他稍微前行，就俯首至地祈禱說：「我父！若是可能，就讓這杯離開我罷！但不要照我，而要照你所願意的。」他來到門徒那裏，見他們睡着了，便對伯多祿說：「你們竟不能同我醒寤一個時辰嗎？醒寤祈禱罷！免陷於誘惑；心神固然切願，但肉體卻軟弱。」他第二次再去祈禱說：「我父！如果這杯不能離去，非要我喝不可，就成就你的意願罷！」他又回來，見他們仍然睡着，因為他們的眼睛很是沈重。他再離開他們，第三次去祈禱，又說了同樣的話。然後回到門徒那裏，對他們說：「你們睡下去罷！休息罷！看，時候到了，人子就要被交於罪人手裏。起來，我們去罷！看，那出賣我的已來近了。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(瑪竇福音 26:36-46)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-chu-nh-n",
+      "name": "二、耶穌受鞭打之刑",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03994914720259146,
+          "pitch": -0.13235010662935842,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-chu-i-mo-gai"
+        },
+        {
+          "yaw": -2.3599925639905557,
+          "pitch": -0.09310296493766934,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-lo-bun--m-hi-mu"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0021049083788469147,
+          "pitch": -0.4432680786781624,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌受鞭打之刑</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">一到清晨，司祭長、長老及經師，和全體公議會商討完畢，就把耶穌捆綁了，解送給比拉多。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">比拉多問他說：「你是猶太人的君王嗎？」耶穌回答說：「你說的是。」司祭長控告他許多事；比拉多又問他說：「你看，他們控告你這麼多的事，你什麼都不回答嗎？」耶穌仍沒有回答什麼，以致比拉多大為驚異。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">每逢節日，總督慣常給民眾釋放一個他們所要求的囚犯。當時，有一個名叫巴辣巴的，他是與那些在暴動中殺人的暴徒一同被囚的。群眾上去，要求照常給他們辦理。比拉多回答他們說：「你們願意我給你們釋放猶太人的君王嗎？」他原知道司祭長是由於嫉妒纔把耶穌解送來的。但是，司祭長卻煽動群眾，寧要給他們釋放巴辣巴。比拉多又向他們說：「那麼，對你們所稱的猶太人君王，我可怎麼辦呢？」他們又喊說：「釘他在十字架上！」比拉多對他們說：「他作了什麼惡事？」他們越發喊說：「釘他在十字架上！」比拉多願意滿足群眾，就給他們釋放了巴辣巴，把耶穌鞭打後，交給他們，釘在十字架上。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(馬爾谷福音 15:1-15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-chu-i-mo-gai",
+      "name": "三、耶穌受茨冠之苦辱",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.770374617292628,
+          "pitch": -0.011154715826149797,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-vc-cy-thnh-gi"
+        },
+        {
+          "yaw": -3.1385039082777517,
+          "pitch": -0.14273786458115012,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-chu-nh-n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7943040273107442,
+          "pitch": -0.21542148670605954,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌受茨冠之苦辱</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">兵士把耶穌帶到庭院裏面，即總督府內，把全隊叫齊，給耶穌穿上紫紅袍，編了一個茨冠給他戴上，開始向他致敬說：「猶太人的君王，萬歲！」然後用一根蘆葦敲他的頭，向他吐唾沫，屈膝朝拜他。他們戲弄了耶穌之後，就給他脫去紫紅袍，給他穿上他自己的衣服，然後帶他出去，把他釘在十字架上。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(馬爾谷福音 15:16-20)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-vc-cy-thnh-gi",
+      "name": "四、耶穌背十字架上山",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6983594634984662,
+          "pitch": -0.37386622268201464,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-chu-cht-trn-cy-thnh-gi"
+        },
+        {
+          "yaw": 3.1374699261000316,
+          "pitch": 0.2915448346724503,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-chu-i-mo-gai"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.66061327614063,
+          "pitch": -0.6363281998126844,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌背十字架上山</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">他們把耶穌帶走的時候，就抓住一個從田間來的基勒乃人西滿，把十字架放在他肩上，叫他在耶穌後面背着。有許多人民及婦女跟隨着耶穌，婦女搥胸痛哭他。耶穌轉身向她們說：「耶路撒冷女子！你們不要哭我，但應哭你們自己及你們的子女，因為日子將到，那時，人要說：那荒胎的，那沒有生產過的胎，和沒有哺養過的乳，是有福的。那時，人要開始對高山說：倒在我們身上罷！對丘陵說：蓋起我們來罷！如果對於青綠的樹木，他們還這樣做，對於枯槁的樹木，又將怎樣呢？」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(路加福音 23:26-31)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-chu-cht-trn-cy-thnh-gi",
+      "name": "五、耶穌死在十字架上",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7763165764238629,
+          "pitch": 0.39931217659419715,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-vc-cy-thnh-gi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4895233345042449,
+          "pitch": -0.6379339964485311,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌死在十字架上</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">於是比拉多把耶穌交給他們去釘死。他們就把耶穌帶去了。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌自己背着十字架出來，到了一個名叫「髑髏」的地方，希伯來話叫「哥耳哥達」，他們就在那裏把他釘在十字架上，同他一起另有兩個人：一個在這邊，一個在那邊，耶穌在中間。比拉多寫了個牌子，放在十字架上端，寫的是：「納匝肋人耶穌，猶太人的君王。」這牌子有許多猶太人唸了，因為耶穌被釘在十字架上的地方離城很近，字是用希伯來、羅馬和希臘文寫的。於是猶太人的司祭長就對比拉多說：「不要寫猶太人的君王，該寫他自己說：我是猶太人的君王。」比拉多答覆說：「我寫了，就寫了。」</b></div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">兵士將耶穌釘在十字架上後，拿了他的衣服，分成四分，每人一分；又拿了長衣，因那長衣是無縫的，由上到下渾然織成，所以他們彼此說：「我們不要把它撕開，我們擲骰，看是誰的。」這就應驗了經上的話：「他們瓜分了我的衣服，為我的長衣，他們拈鬮。」士兵果然這樣作了。</b></div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">在耶穌的十字架傍，站着他的母親和他母親的姊妹，還有克羅帕的妻子瑪利亞和瑪利亞瑪達肋納。耶穌看見母親，又看見他所愛的門徒站在旁邊，就對母親說：「女人，看，你的兒子！」然後，又對那門徒說：「看，你的母親！」就從那時起，那門徒把她接到自己家裏。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">此後，耶穌因知道一切事都完成了，為應驗經上的話，遂說：「我渴。」有一個盛滿了醋的器皿放在那裏，有人便將海綿浸滿了醋，綁在長槍上，送到他的口邊。耶穌一嚐了那醋，便說：「完成了。」就低下頭，交付了靈魂。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(若望福音 19:16-30)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
